4f6d5efba064d92ab7ee99a992c7603c491f5f8b
Fine974 <1370376731@qq.com>
2025-02-22 10:06:48 +0800

    导出报表业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @RestController
@@ -85,5 +86,15 @@ public class ReportController {
             @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate end) {
         log.info("销量排名top10：{},{}",begin, end);
         return Result.success(reportService.getSalesTop10(begin, end));
+    }
+
+    /**
+     * 导出运营数据
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据")
+    public void exportBusinessData(HttpServletResponse response) {
+        reportService.exportBusinessData(response);
     }
 }
